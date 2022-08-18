142b6bc5381f02516fc1f3f6d953f6100874323d
Anupam Dalmia <anupam.dalmia@logicmonitor.com>
2022-08-18 15:08:08 +0530

    feat: add local rate limiting

--- src/api/logs/Logs.ts
+++ src/api/logs/Logs.ts
@@ -6,16 +6,20 @@ export class Log {
     public company:string;
     public accessID:string;
     public accessKey:string;
+    private requestCount: number;
+    private rateLimitInitTime!: number;
+    private readonly oneMinute: number = 60000;
     readonly resourcePath = "/log/ingest";
     readonly url:string;
     readonly batch:boolean;
     readonly interval:number;
+    private readonly rateLimitPerMinute: number;
     readonly mutex:MutexInterface;
     private logBatch = [] as any;
     ticker: () => void;
     tickerID: NodeJS.Timer | null;
 
-    constructor(batch = false, interval = 10) {
+    constructor(batch = false, interval = 10, rateLimitPerMinute = 100) {
         this.company = process.env.LM_COMPANY!;
         this.accessID = process.env.LM_ACCESS_ID!;
         this.accessKey = process.env.LM_ACCESS_KEY!;
@@ -28,6 +32,8 @@ export class Log {
 
         this.batch = batch;
         this.interval = interval;
+        this.rateLimitPerMinute = rateLimitPerMinute;
+        this.requestCount = 0;
         this.tickerID = null;
         this.ticker = () => {
             this.tickerID = setInterval(() => {
@@ -71,6 +77,16 @@ export class Log {
             this.logBatch = [];
         }   finally {
             release();
+        }
+
+          //check rate limit
+          if(this.requestCount === 0 || Date.now() - this.rateLimitInitTime > this.oneMinute){
+            this.requestCount = 1;
+            this.rateLimitInitTime = Date.now();
+        } else if(Date.now() - this.rateLimitInitTime <= this.oneMinute && this.requestCount > this.rateLimitPerMinute){
+            throw new Error('The number of requests exceeds the rate limit');
+        } else{
+            this.requestCount += 1;
         }
         const response = await makeRequest('POST', this.url, this.resourcePath, body, this.accessID, this.accessKey);
 
